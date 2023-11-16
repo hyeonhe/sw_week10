8c9e965464b27eeffdb6d38a2fa7289a51d90526
dcan <adc040313@gmail.com>
2023-11-16 16:11:36 +0900

    main.py

--- main.py
+++ main.py
@@ -1,41 +1,11 @@
-def to_uppercase(input_string):
-    return input_string.upper()
+def 짝수_확인(숫자):
+    return 숫자 % 2 == 0
 
-# 예제 사용
-original_string = "hello, world!"
-uppercase_string = to_uppercase(original_string)
-print(uppercase_string)
+# 사용 예시
+입력_숫자 = int(input("숫자를 입력하세요: "))
 
-def add_numbers(num1, num2):
-    return num1 + num2
-
-# 예제 사용
-result = add_numbers(3, 5)
-print(result)
-
-def is_even(number):
-    return number % 2 == 0
-
-# 예제 사용
-number_to_check = 4
-if is_even(number_to_check):
-    print(f"{number_to_check} is even.")
+if 짝수_확인(입력_숫자):
+    print(f"{입력_숫자}는 짝수입니다.")
 else:
-    print(f"{number_to_check} is odd.")
+    print(f"{입력_숫자}는 홀수입니다.")
 
-def concatenate_strings(str1, str2):
-    return str1 + str2
-
-# 예제 사용
-string1 = "Hello, "
-string2 = "world!"
-concatenated_string = concatenate_strings(string1, string2)
-print(concatenated_string)
-
-def count_elements(input_list):
-    return len(input_list)
-
-# 예제 사용
-my_list = [1, 2, 3, 4, 5]
-element_count = count_elements(my_list)
-print(element_count)
